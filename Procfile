1127d66f3a7d6edebcb58a78aa3a256d126a41d6
Aurora <aurorastorm22@proton.me>
2023-07-18 18:59:02 +0000

    Download django and cloudinary libraries, deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn inspired_ink.wsgi
--- inspired_ink/settings.py
+++ inspired_ink/settings.py
@@ -20,6 +20,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -33,6 +34,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-aurorastorm-inspiredink-han0133genl.ws-eu101.gitpod.io',
+    'https://inspired-ink-3acb52d766db.herokuapp.com/',
+    'localhost',
 ]
 
 
@@ -44,7 +47,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'blog',
 ]
 
@@ -63,7 +68,7 @@ ROOT_URLCONF = 'inspired_ink.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -131,6 +136,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
