8ed16a9b9cbb3c006fa3646d3d5231ee67af65d4
Aurora <aurorastorm22@proton.me>
2023-07-24 06:24:32 +0000

    Successfully add form to add_post.html for post posting

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,22 @@
-from .models import Comment
+from .models import Comment, Post
 from django import forms
 
 
+class AddInk_form(forms.ModelForm):
+    """
+    Form model that allows authenticated users to
+    write and post posts
+    """
+    class Meta:
+        model = Post
+        fields = ('title', 'content',)
+
+
 class FeedbackForm(forms.ModelForm):
+    """
+    Form model that allows authenticated users to
+    leave feedback on posts
+    """
     class Meta:
         model = Comment
         fields = ('body',)
