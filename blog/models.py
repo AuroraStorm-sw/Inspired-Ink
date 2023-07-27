5f8c8953fe957147393c05dfff291e774e5e9f12
Aurora <aurorastorm22@proton.me>
2023-07-27 05:33:29 +0000

    Add model and admin register for Category

--- blog/models.py
+++ blog/models.py
@@ -61,3 +61,7 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=80)
